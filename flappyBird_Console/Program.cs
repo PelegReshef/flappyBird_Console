0ea9f57efc8f0db320b33eb8d7d395d075bb7459
user1 <user1@LAB122PC10>
2025-12-26 09:51:18 +0200

    addaed pipes

--- flappyBird_Console/Program.cs
+++ flappyBird_Console/Program.cs
@@ -26,9 +26,11 @@
             Print();
             Console.ReadKey(true);
             Thread.Sleep(1000);
+            List<Pipe> pipes = new List<Pipe>();
+            int pipeCounter = 20;
             while (true)
             {
-                Thread.Sleep(20);
+                Thread.Sleep(30);
                 Delete();
 
                 if (Console.KeyAvailable)
@@ -52,7 +54,90 @@
                 Console.Write(Math.Round(v, 3).ToString() + " , " + Math.Round(y, 3).ToString());
 
                 Print();
+                if (pipeCounter % 1 == 0)
+                {
+                    foreach (Pipe p in pipes)
+                    {
+                        if (p.x <= 1)
+                        {
+                            p.Delete();
+                            continue;
+                        }
+                        p.Move();
+                    }
+
+                }
+                pipes.RemoveAll(pipe => pipe.x <= 0);
+
+                if (pipeCounter == 60)
+                {
+                    pipes.Add(new Pipe(Console.WindowWidth /2 - 1));
+                    pipeCounter = 0;
+                }
+                pipeCounter++;
             }
+        }
+    }
+    class Pipe
+    {
+        Random r = new Random();
+        public int x;
+        int y;
+        int gapTop;
+        int gapBottom;
+
+        const int GapSize = 14;
+        const string Icon = "###";
+
+        public Pipe(int x)
+        {
+            this.x = x;
+            (int minRange, int maxRange) = GetWindowBorders();
+            int gapY = r.Next(minRange, maxRange + 1);
+            gapTop = gapY + GapSize / 2;
+            gapBottom = gapY - GapSize / 2;
+
+
+        }
+        (int, int) GetWindowBorders()
+        {
+            int consoleMax = Console.WindowHeight;
+            int consoleMin = 0;
+
+            int min = consoleMin + (int)Math.Ceiling(GapSize / 2.0);
+            int max = consoleMax - (int)Math.Ceiling(GapSize / 2.0);
+            return (min,  max);
+        }
+        void Print()
+        {
+            for (int i = 0; i < Console.WindowHeight;  i++)
+            {
+                Console.SetCursorPosition(x, i);
+                if (i > gapBottom && i < gapTop)
+                {
+                    Console.Write("   ");
+                }
+                else
+                {
+                    Console.Write(Icon);
+                }
+
+            }
+        }
+        public void Delete()
+        {
+            for (int i = 0; i < Console.WindowHeight; i++)
+            {
+                Console.SetCursorPosition(x, i);
+                Console.Write("   ");
+            }
+
+        }
+        public void Move()
+        {
+            Delete();
+            x--;
+            Print();
         }
     }
 }
